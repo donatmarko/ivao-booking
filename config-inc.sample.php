7a39681e329df002eac528d53a9502d7def2045e
Donat MARKO <donat.marko@donatus.hu>
2025-01-02 01:35:56 +0100

    chore: updates sample config

--- config-inc.sample.php
+++ config-inc.sample.php
@@ -4,25 +4,24 @@
  * Created by Donat Marko (IVAO VID 540147) 
  * Any artwork/content displayed on IVAO is understood to comply with the IVAO Creative Intellectual Property Policy (https://wiki.ivao.aero/en/home/ivao/intellectual-property-policy)
  * @author Donat Marko
- * @copyright 2024 Donat Marko | www.donatus.hu
+ * @copyright 2025 Donat Marko | www.donatus.hu
  */
 
 /**
  * SQL server settings.
- * We're using separate database for booking and navigational (airlines, airfields, aircraft) data.
- * The database user shall have read/write access to the booking, and at least read access to the navigational database.
+ * We now use one single database for booking and navigational (airlines, airfields, aircraft) data.
+ * The database user shall have read/write access to the database.
  */
 define('SQL_SERVER',       'localhost');
 define('SQL_USERNAME',     'booking');
 define('SQL_PASSWORD',     'ITSYOURPASSWORD');
 define('SQL_DATABASE',     'booking');
-define('SQL_DATABASE_NAV', 'booking_nav');
 
 /**
  * URL of the booking system (full path).
  * Needed for the IVAO login API and to the emails.
  */
-define('SITE_URL',         'https://booking.hu.ivao.aero/');
+define('SITE_URL',         'https://booking.va.ivao.aero/');
 
 /**
  * Maintenance mode.
@@ -37,18 +36,23 @@ $config["login_bypass_api"]   = false;
  */
 define('IVAOSSO_CLIENT_ID',      'your-client-id');
 define('IVAOSSO_CLIENT_SECRET',  'your-client-secret');
-define('IVAOSSO_REDIRECT_URI',   'https://booking.hu.ivao.aero/auth/callback');
+define('IVAOSSO_REDIRECT_URI',   'https://booking.va.ivao.aero/auth/callback');
 
 /**
  * Email settings.
  */
 $config["mail_enabled"]       = true;
-$config["mail_from_name"]     = 'IVAO Vatican booking system';
-$config["mail_from_email"]    = 'events@ivao.va';
+$config["mail_mode"]          = 'smtp';  // can be smtp or sendmail
 
-$config["mail_smtp_server"]   = 'mail.example.com';
-$config["mail_smtp_port"]     = 587;
+$config["mail_from_name"]     = 'IVAO Vatican Booking System';
+$config["mail_from_email"]    = 'rfe@booking.va.ivao.aero';
+
+$config["mail_replyto_name"]  = 'IVAO Vatican Staff';
+$config["mail_replyto_email"] = 'va-staff@ivao.aero';
+
+$config["mail_smtp_server"]   = 'divisions.ivao.aero';
+$config["mail_smtp_port"]     = 465;
 $config["mail_smtp_auth"]     = true;
-$config["mail_smtp_secure"]   = 'tls';
-$config["mail_smtp_username"] = 'smtp@api.example.com';
+$config["mail_smtp_secure"]   = 'ssl';
+$config["mail_smtp_username"] = 'rfe@booking.va.ivao.aero';
 $config["mail_smtp_password"] = 'ITSYOURPASSWORD';
